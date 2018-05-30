1bdae4bfb0d11fac566bc7479247b736936d3406
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2018-05-30 13:59:43 -0600

    proyectoM

---
+++ mainP.py
@@ -0,0 +1,75 @@
+#Mario Sarmientos 17055
+
+
+from proyecto import *
+from random import *
+print("Musical Recomendations")
+
+#loop de programa
+cond = False
+while (cond == False):
+    print("")
+    print("1. Ingresar una cancion!")
+    print("2. Buscar por genero")
+    print("3. Buscar por estado de animo")
+    print("4. Buscar por artista")
+    print("5. Buscar cancion especifica")
+    print("6. Salir")
+    opc = (input("elija que desea hacer: "))
+
+#acciones a ejecutar de acorde al usario
+    if opc == "1":
+        nombre = input("nombre de la cancion:")
+        artista = input("nombre del artista: ")
+        genero  = input("genero musical: ")
+        estado = input("estado de animo")
+        nuevaCancion(nombre,artista,genero,estado)
+        
+    if opc == "2":
+        genero = input("De que genero desea encontrar canciones ")
+        genero = str(genero)
+        encontrarGenero(genero)
+        
+    if opc == "3":
+        state = input("de que estado de animo te sientes? ")
+        state = str(state)
+        encontrarEstado(state)
+    if opc == "4":
+        artist = input("De que artista desea escuchar musica? ")
+        artist = str(artist)
+        encontrarArtista(artist)
+    if opc == "5":
+        song = input("Que cancion te gustaria escuchar? ")
+        song = str(song)
+        encontrarCancion(song)
+        recomendacion = randint(1, 3)
+        print("\n Estas canciones tambien te pueden gustar!!!\n")
+        
+        #recomendacion de canciones por genero
+        if recomendacion == 1:
+            encontrarGenero(str(ExtraerGenero(song)))
+        #recomendacion de canciones por artista
+        if recomendacion == 2:
+            
+            encontrarArtista(str(ExtraerArtista(song)))
+        #recomendacion de canciones por estado    
+        if recomendacion == 3:
+            
+            encontrarEstado(str(ExtraerEstado(song)))
+
+
+    respuesta = input("Desea seguir? S/N ")
+    while (respuesta != "S" and respuesta != "N"):
+        print ("Por favor ingrese uno de los siguientes(S o N)")
+        respuesta = input("Desea realizar otra accion? S/N")
+
+    if (respuesta == "S"):
+        cond == False
+    else:
+        Salir();
+        cond = True
+        print ("Gracias por usar el programa, esperamos verlo pronto.")
+        
+        
+
+
---
+++ proyecto.py
@@ -0,0 +1,95 @@
+#Mario Sarmientos 17055 }
+
+from neo4j.v1 import GraphDatabase, basic_auth
+
+#Establece el driver y la sesion de Neo4J
+driver = GraphDatabase.driver("bolt:localhost:7474", auth=basic_auth("neo4j", "123"))#AGREGAR USERNAME Y PASSWORD PARA ACCESAR A NE0EJ.
+session = driver.session()
+
+#funcion para agregar canciones a la bd
+def nuevaCancion(nombre,artista,genero,estado):
+    session.run("CREATE (a:Cancion {nombre: {nombre}, artista: {artista}, genero: {genero},estado: {estado}})",
+                {"nombre": nombre, "artista": artista, "genero": genero, "estado":estado})
+
+#funcion para encontrar canciones por genero
+
+def encontrarGenero(genero):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.genero = {genero}"
+                          "RETURN song.nombre AS nombre, song.artista AS artista",
+                          
+                          {"genero":genero})
+    print("\n")
+    print("***estas canciones de acorde al genero podrian gustarte***\n")
+    for dato in cancion:
+        print(dato["nombre"] + " Artista: " + dato["artista"]+ " Genero: "+ genero )
+
+#funcion automatica que genera los estados en la bd
+
+#def crearEstado():
+#    session.run("CREATE (a:Estado {estado:{State}}))",
+#                {"State":"Felicidad"})
+#    session.run("CREATE (a:Estado {estado:{State}}))",
+#                {"State":"Tristesa"})
+#    session.run("CREATE (a:Estado {estado:{State}}))",
+#                {"State":"Extremo"})
+                
+
+#funcion para asociar canciones a estados de animo
+#def asociacionGen(cancion,estado):
+#    session.run("MATCH (cancion:Cancion),(estado:Estado)"
+#                "WHERE cancion.nombre = {cancion} AND  estado.estado = {estado}"
+#                "CREATE (cancion)-[:ESTADO] -> (estado)",
+#                {"cancion":cancion,"estado":estado})
+
+#funcion para encontrar canciones por estado de animo
+def encontrarEstado(state):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.estado = {state}"
+                "RETURN song.nombre AS nombre, song.artista AS artista, song.genero AS genero",
+                {"state":state})
+    print("\n")  
+    print("***estas canciones de acorde a tu estado de animo podrian gustarte***\n")
+    for dato in cancion:
+        print ("Nombre: " + dato["nombre"] +" Artista: "+ dato["artista"] + " Genero: " + dato["genero"] + " Estado: " + state)
+
+#funcion para encontrar canciones por artista
+def encontrarArtista(artist):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.artista = {artist}"
+                "RETURN song.nombre AS nombre, song.genero AS genero, song.estado AS estado",
+                {"artist":artist})
+    for dato in cancion:
+        print ("Nombre: " + dato["nombre"] +" Artista: "+ artist + " Genero: " + dato["genero"] + " Estado: " + dato["estado"])
+#funcion para encontrar cancion
+def encontrarCancion(song):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.nombre = {name}"
+                          "RETURN song.nombre AS nombre, song.artista AS artista, song.genero AS genero",
+                          {"name":song})
+    print("resultados para la busqueda: " + song + "\n")
+    for dato in cancion:
+        print("Nombre: " + song + " Artista: " + dato["artista"] + " Genero: "+dato["genero"]) 
+#Extraer genero de cancion
+def ExtraerGenero(song):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.nombre = {name}"
+                          "RETURN song.genero AS genero",{"name":song})
+    for info in cancion:
+        genere = info["genero"]
+    return genere
+
+#Extrae el estado de una cancion
+def ExtraerEstado(song):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.nombre = {name}"
+                          "RETURN song.estado AS estado",{"name":song})
+    for info in cancion:
+        genere = info["estado"]
+    return genere
+
+#Extrae el artista de una cancion
+def ExtraerArtista(song):
+    cancion = session.run("MATCH (song:Cancion) WHERE song.nombre = {name}"
+                          "RETURN song.artista AS artista",{"name":song})
+    for info in cancion:
+        genere = info["artista"]
+    return genere 
+
+#funcion para terminar conexion con bd
+def Salir():
+    session.close()
